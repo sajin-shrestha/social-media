fe4e7fee4526d62225b30c2a01fdbf1c9af03dce
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-04-30 19:54:04 +0545

    create backend for likes and comment

--- server/index.js
+++ server/index.js
@@ -1,57 +1,64 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-import multer from "multer";
-import helmet from "helmet";
-import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
-import authRoutes from "./routes/auth.js"
-import { register } from "./controllers/auth.js";
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+import helmet from 'helmet'
+import mongoose from 'mongoose'
+import morgan from 'morgan'
+import multer from 'multer'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { register } from './controllers/auth.js'
+import { createPost } from './controllers/posts.js'
+import { verifyToken } from './middleware/auth.js'
+import authRoutes from './routes/auth.js'
+import postRoutes from './routes/posts.js'
+import userRoutes from './routes/users.js'
 
 // Configuration //
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+dotenv.config()
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("commom"));
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+app.use(morgan('commom'))
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
 
 // File Storage //
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/assets");
+    cb(null, 'public/assets')
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, file.originalname)
   },
-});
-const upload = multer({ storage });
+})
+const upload = multer({ storage })
 
 // Routes With Files //
-app.post("/auth/register", upload.single("picture"), register);
+app.post('/auth/register', upload.single('picture'), register)
+app.post('/posts', verifyToken, upload.single('picture'), createPost)
 
 // Routes //
-app.use("/auth", authRoutes);
+app.use('/auth', authRoutes)
+app.use('/users', userRoutes)
+app.use('/posts', postRoutes)
 
 // Mongoose setup //
-const PORT = process.env.PORT || 6001;
+const PORT = process.env.PORT || 6001
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(PORT, () => console.log(`Running on Server Port: ${PORT}`));
+    app.listen(PORT, () => console.log(`Running on Server Port: ${PORT}`))
   })
-  .catch((error) => console.log(`${error} did not connect`));
+  .catch((error) => console.log(`${error} did not connect`))
